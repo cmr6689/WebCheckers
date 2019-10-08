5231bb5425ad1dcbb7a7240be4be4f75e2344459
rt3115@rit.edu <Maggie4182>
2019-10-08 09:25:00 -0400

    removed test message

--- src/main/java/com/webcheckers/ui/GetSignInRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -52,7 +52,7 @@ public class GetSignInRoute implements Route {
 
     // display a user message in the Home page
     vm.put("message", WELCOME_MSG);
-
+    System.err.println(vm.get("myText"));
 
     // render the View
     return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
