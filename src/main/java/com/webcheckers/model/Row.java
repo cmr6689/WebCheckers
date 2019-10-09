35ad8ba4f15f98ea8d181967bb97435092516ea5
evanprice <esp9443@rit.edu>
2019-10-09 16:36:31 -0400

    Very minor fix for Row.

--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -11,7 +11,7 @@ public class Row {
     public Row(int index){
         this.index = index;
         spacesArray = new Space[8];
-        for(int i = 0; i <= 7; i++){
+        for(int i = 0; i < 8; i++){
             Space space = new Space(null,i);
             spacesArray[i] = space;
         }
