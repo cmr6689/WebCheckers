fee7a7890555854e7edd7c0a82a3e2bdbb88ab2b
rt3115@rit.edu <Maggie4182>
2019-10-17 09:15:22 -0400

    added baordview to gameCenter

--- src/main/java/com/webcheckers/application/GameCenter.java
+++ src/main/java/com/webcheckers/application/GameCenter.java
@@ -1,5 +1,16 @@
 package com.webcheckers.application;
 
+import com.webcheckers.model.BoardView;
+
 public class GameCenter {
 
+    private BoardView boardView;
+
+    public GameCenter (){
+        boardView = new BoardView();
+    }
+
+    public BoardView getBoardView() {
+        return boardView;
+    }
 }
