6cee08087d3d33aa7a11089f6e0be7864c4a131a
rt3115@rit.edu <Maggie4182>
2019-10-17 13:33:54 -0400

    move gamecenter code to game

--- src/main/java/com/webcheckers/application/GameCenter.java
+++ src/main/java/com/webcheckers/application/GameCenter.java
@@ -5,42 +5,4 @@ import com.webcheckers.model.Player;
 
 public class GameCenter {
 
-    private Player player1;
-    private Player player2;
-    private BoardView boardView;
-
-    public GameCenter (){
-        //boardView = new BoardView();
-    }
-
-    public BoardView getBoardView() {
-        return boardView;
-    }
-
-    public void startGame(){
-        boardView = new BoardView();
-    }
-
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
-
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
-
-    public void setPlayer(Player p){
-        if(player1 == null)
-            player2 = p;
-        else
-            player1 = p;
-    }
 }
