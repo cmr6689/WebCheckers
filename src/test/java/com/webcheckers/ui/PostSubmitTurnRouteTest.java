125072553133f773f76311ed8ce6a1acc5be95e9
rt3115@rit.edu <Maggie4182>
2019-11-04 19:56:25 -0500

    I think I finished the PostSubmitTurnRouteTest

--- src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
@@ -3,6 +3,8 @@ package com.webcheckers.ui;
 import com.google.gson.Gson;
 import com.webcheckers.application.GameCenter;
 import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.model.BoardView;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.Request;
@@ -24,6 +26,9 @@ class PostSubmitTurnRouteTest {
     private Request request;
     private Response response;
 
+    Player pl1 = new Player("1");
+    Player pl2 = new Player("2");
+
     private PostSubmitTurnRoute CuT;
 
     @BeforeEach
@@ -35,6 +40,20 @@ class PostSubmitTurnRouteTest {
         playerLobby = new PlayerLobby(new GameCenter());
         gameData = new GameData();
         CuT = new PostSubmitTurnRoute(gameData, playerLobby);
+
+        gameData.setCurrentUser(pl1);
+        gameData.setViewMode(null);
+        gameData.setRedPlayer(pl1);
+        gameData.setWhitePlayer(pl2);
+        gameData.setBoard(new BoardView(1));
+        playerLobby.addPlayer(pl1);
+        playerLobby.addPlayer(pl2);
+        playerLobby.getGameCenter().newGame(pl1, pl2);
+
+        final Session httpSession = request.session();
+        when(session.attribute("player")).thenReturn(pl1);
+        httpSession.attribute("player", pl1);
+
     }
 
     @Test
@@ -45,7 +64,32 @@ class PostSubmitTurnRouteTest {
     }
 
     @Test
-    void handle() {
+    public void changeActiveToRed(){
 
+        gameData.setActiveColor("WHITE");
+        gameData.dataSetup();
+        CuT.handle(request, response);
+        assertEquals("RED", gameData.getVm().get("activeColor"));
+    }
+
+    @Test
+    public void changeActiveToWhite(){
+        gameData.setActiveColor("RED");
+        gameData.dataSetup();
+        CuT.handle(request, response);
+        assertEquals("WHITE", gameData.getVm().get("activeColor"));
+    }
+
+    @Test
+    public void messageIsInfo(){
+
+        gameData.setActiveColor("RED");
+        gameData.dataSetup();
+        ResponseMessage message = new ResponseMessage();
+        message.setType(ResponseMessage.MessageType.INFO);
+        message.setText("You can not submit a turn in the state you are in.");
+        Gson gson = new Gson();
+
+        assertEquals(gson.toJson(message), CuT.handle(request, response));
     }
 }
