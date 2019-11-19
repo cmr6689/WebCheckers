5eb66170715af5e340148f89c3490c0f45d7bd4c
Matthew Klein <mk2995@g.rit.edu>
2019-11-19 16:16:52 -0500

    AI can move right

--- src/main/java/com/webcheckers/ui/AIHandler.java
+++ src/main/java/com/webcheckers/ui/AIHandler.java
@@ -10,6 +10,7 @@ public class AIHandler {
 
     private PlayerLobby playerLobby;
     private Player myPlayer;
+    private boolean done = false;
 
     public AIHandler(PlayerLobby playerLobby, Player myPlayer){
         this.playerLobby = playerLobby;
@@ -22,24 +23,26 @@ public class AIHandler {
         for (int i = 0; i < 8; i++) {
             //loop through spaces
             for (int j = 0; j < 8; j++) {
-                Position position = new Position(i, j);
-                if (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece() != null &&
-                        (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE))
-                                || playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.KING)))) {
-                    if (moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
-                            position).size() > 0) {
-                        Move jump = moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
-                                position).get(0);
-                        playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getStart().getRow()).getSpaceAtIndex(jump.getStart().getCell()).removePiece();
-                        playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getEnd().getRow()).getSpaceAtIndex(jump.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
-                        break;
-                    } else if (moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
-                            position).size() > 0) {
-                        Move move = moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
-                                position).get(0);
-                        playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getStart().getRow()).getSpaceAtIndex(move.getStart().getCell()).removePiece();
-                        playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getEnd().getRow()).getSpaceAtIndex(move.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
-                        break;
+                if (!done) {
+                    Position position = new Position(i, j);
+                    if (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece() != null &&
+                            (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE))
+                                    || playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.KING)))) {
+                        if (moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                position).size() > 0) {
+                            Move jump = moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                    position).get(0);
+                            playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getStart().getRow()).getSpaceAtIndex(jump.getStart().getCell()).removePiece();
+                            playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getEnd().getRow()).getSpaceAtIndex(jump.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
+                            done = true;
+                        } else if (moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                position).size() > 0) {
+                            Move move = moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                    position).get(0);
+                            playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getStart().getRow()).getSpaceAtIndex(move.getStart().getCell()).removePiece();
+                            playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getEnd().getRow()).getSpaceAtIndex(move.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
+                            done = true;
+                        }
                     }
                 }
             }
