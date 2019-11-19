a2df704566491215beb0c85ac48aff558f5fc4f0
Matthew Klein <mk2995@g.rit.edu>
2019-11-18 23:11:03 -0500

    AI has moving pieces

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -2,7 +2,10 @@ package com.webcheckers.ui;
 
 import com.google.gson.Gson;
 import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.model.Move;
+import com.webcheckers.model.Piece;
 import com.webcheckers.model.Player;
+import com.webcheckers.model.Position;
 import com.webcheckers.util.Message;
 import spark.*;
 
@@ -47,9 +50,33 @@ public class PostCheckTurnRoute implements Route {
         Player myPlayer = httpSession.attribute("player");
         LOG.config("PostCheckTurn is invoked by " + myPlayer.getName() + ".");
 
-        MoveChecks move = new MoveChecks(playerLobby.getGameCenter().getGame(myPlayer));
-
         if(playerLobby.getGameCenter().getGame(myPlayer) != null && playerLobby.getGameCenter().getGame(myPlayer).getPlayer2().getName().equals("AI")){
+            MoveChecks moveCheck = new MoveChecks(playerLobby.getGameCenter().getGame(myPlayer));
+                //loop through rows
+                for (int i = 0; i < 8; i++) {
+                    //loop through spaces
+                    for (int j = 0; j < 8; j++) {
+                        if (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece() != null &&
+                                (playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE))
+                                        || playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece().equals(new Piece(Piece.COLOR.WHITE, Piece.TYPE.KING)))) {
+                            if (moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                    new Position(i, j)).size() > 0) {
+                                Move jump = moveCheck.checkSinglePieceJumps(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                        new Position(i, j)).get(0);
+                                playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getEnd().getRow()).getSpaceAtIndex(jump.getEnd().getCell()).removePiece();
+                                playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(jump.getEnd().getRow()).getSpaceAtIndex(jump.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
+                                break;
+                            } else if (moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                    new Position(i, j)).size() > 0) {
+                                Move move = moveCheck.checkSinglePieceMoves(playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(i).getSpaceAtIndex(j).getPiece(),
+                                        new Position(i, j)).get(0);
+                                playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getStart().getRow()).getSpaceAtIndex(move.getStart().getCell()).removePiece();
+                                playerLobby.getGame(myPlayer).getBoardView1().getRowAtIndex(move.getEnd().getRow()).getSpaceAtIndex(move.getEnd().getCell()).setPiece(new Piece(Piece.COLOR.WHITE, Piece.TYPE.SINGLE));
+                                break;
+                            }
+                        }
+                    }
+                }
 
             playerLobby.getGameCenter().getGame(myPlayer).getMap().put("activeColor", "RED");
         }
